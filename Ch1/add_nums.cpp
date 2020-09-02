d92fe61f8a2de9f60eeefc5abe75bff5c08f4aa1
Morgan Rose Sterling <morgan.rose.sterling@gmail.com>
2020-09-02 17:10:47 -0400

    Added work for 9/2/20

---
+++ Ch1/add_nums.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x1;
+    int x2;
+    
+    cout << "Enter two numbers to be added." << endl;
+    cin >> x1 >> x2;
+    cout << x1 << " + " << x2 << " = " << x1 + x2 << endl;
+}
+
---
+++ Ch1/name.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // read your name from the keyboard and output your name
+    string name;
+    int age;
+    
+    cout << "What is your name?" << endl;
+    cin  >> name;
+    cout << "Hello, " << name << "!" << endl;
+}
+
